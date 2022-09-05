c02c6c3b63a935d2576e5039d729027406e65148
esayyas <esayyasahemd1012@gmailc.om>
2022-09-05 23:36:10 +0300

    test_console.py

---
+++ tests/test_console.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" test for console"""
+
+import sys
+import models
+import unittest
+from io import StringIO
+from console import HBNBCommand
+from unittest.mock import create_autospec
+
+
+class test_console(unittest.TestCase):
+    ''' Test the console module'''
+    def setUp(self):
+        '''setup for'''
+        self.backup = sys.stdout
+        self.capt_out = StringIO()
+        sys.stdout = self.capt_out
+
+    def tearDown(self):
+        ''''''
+        sys.stdout = self.backup
+
+    def create(self):
+        ''' create an instance of the HBNBCommand class'''
+        return HBNBCommand()
+
+    def test_quit(self):
+        ''' Test quit exists'''
+        console = self.create()
+        self.assertTrue(console.onecmd("quit"))
+
+
